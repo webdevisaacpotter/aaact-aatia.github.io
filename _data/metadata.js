c6b9961c5f07a51aa45e8582aa811c8081734d06
Shawn Thompson <plansmash@me.com>
2024-05-10 13:32:48 -0400

    update the metadat file

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,16 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
-	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	title: {
+		en: "Accessibility, Accommodation and Adaptive Computer Technology (AAACT)",
+		fr: "Accessibilité, adaptation et technologie informatique adaptée (AATIA)"
+	},
+	url: "https://aaact-aatia.github.io/",
+	description: {
+		en: "The Government of Canada's Accessibility, Accommodation and Adaptive Computer Technology (AAACT) program aims to lead in creating inclusive, barrier-free workplaces and services by providing support, training, and resources to ensure accessible digital content and adaptive technologies for all public servants, including those with disabilities.",
+		fr: "Le programme d'Accessibilité, d'Adaptation et de Technologie Informatique Adaptée(AATIA) du gouvernement du Canada vise à être un leader mondial dans la création de lieux de travail et de services inclusifs et sans obstacles.Il offre soutien, formation et ressources pour assurer que les contenus numériques et les technologies adaptatives soient accessibles à tous les fonctionnaires, y compris ceux ayant des handicaps."
+	},
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "AAACT / AATIA (SSC/SPC)",
+		email: "aaact-aatia@ssc-spc.gc.ca",
+		url: "https://aaact-aatia.github.io/"
 	}
 }
