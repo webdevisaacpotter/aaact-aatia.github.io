7f6986c3188a0e6f38a5f550ffcd0f454d019c7a
Shawn Thompson <plansmash@me.com>
2024-06-25 15:32:28 -0400

    i18n eventList

---
+++ _data/events.js
@@ -0,0 +1,24 @@
+module.exports = {
+	fr: {
+		location: "Lieu :",
+		online: "En ligne",
+		hostedBy: "Présenté par",
+		startTime: "Heure de début de l'événement",
+		endTime: "Heure de fin de l'événement",
+		eventDate: "Date de l'événement",
+		language: "Langue de la présentation",
+		audience: "Audience",
+		eventLocation: "Lieu de l'événement",
+	},
+	en: {
+		location: "Location:",
+		online: "Online",
+		hostedBy: "Presented by",
+		startTime: "Event start time",
+		endTime: "Event end time",
+		eventDate: "Event date",
+		language: "Language of presentation",
+		audience: "Audience",
+		eventLocation: "Location",
+	},
+};
