dbd07896f078b5285ff31070dd11bf2fb9cef1b3
Shawn Thompson <plansmash@me.com>
2025-01-30 15:18:50 -0500

    add menu to site

---
+++ _data/menu.js
@@ -0,0 +1,17 @@
+module.exports = {
+	fr: {
+		mainMenu: "principal",
+		home: "Accueil",
+		podcasts: "Podcasts",
+		events: "Evenements",
+		newsletters: "Bulletins d'information",
+	},
+
+	en: {
+		mainMenu: "main",
+		home: "Home",
+		podcasts: "Podcasts",
+		events: "Events",
+		newsletters: "Newsletters",
+	},
+};
