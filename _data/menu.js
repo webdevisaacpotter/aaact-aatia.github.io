ed1265a1689b87ffe2a47bf18c99e33650fcee78
Shawn Thompson <plansmash@me.com>
2025-02-20 15:11:02 -0500

    refactor menu structure to use objects for links and titles

--- _data/menu.js
+++ _data/menu.js
@@ -1,17 +1,41 @@
 module.exports = {
 	fr: {
 		mainMenu: "principal",
-		home: "Accueil",
-		podcasts: "Podcasts",
-		events: "Evenements",
-		newsletters: "Bulletins d'information",
+		home: [{
+			title: "Accueil",
+			link: "/fr/"
+		}],
+		podcasts: [{
+			title: "Podcasts",
+			link: "/fr/podcasts"
+		}],
+		events: [{
+			title: "Evenements",
+			link: "/fr/evenements"
+		}],
+		newsletters: [{
+			title: "Infolettres",
+			link: "/fr/infolettres"
+		}],
 	},
 
 	en: {
 		mainMenu: "main",
-		home: "Home",
-		podcasts: "Podcasts",
-		events: "Events",
-		newsletters: "Newsletters",
+		home: [{
+			title: "Home",
+			link: "/en/"
+		}],
+		podcasts: [{
+			title: "Podcasts",
+			link: "/en/podcasts/"
+		}],
+		events: [{
+			title: "Events",
+			link: "/en/events/"
+		}],
+		newsletters: [{
+			title: "Newsletters",
+			link: "/en/newsletters/"
+		}],
 	},
 };
